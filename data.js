c09bff317496a3c09dfca3e2970b801c47e73c74
wendy <492137544@qq.com>
2026-06-26 07:38:52 +0800

    ss

--- data.js
+++ data.js
@@ -1,26 +1,5 @@
 window.TECHNICIANS = [
-  {
-    "number": "118",
-    "note": "照片 2 · 视频 1",
-    "avatar": "assets/media/118-1.jpg",
-    "media": [
-      {
-        "type": "image",
-        "title": "118 照片 1",
-        "src": "assets/media/118-1.jpg"
-      },
-      {
-        "type": "image",
-        "title": "118 照片 2",
-        "src": "assets/media/118-2.jpg"
-      },
-      {
-        "type": "video",
-        "title": "118 视频",
-        "src": "assets/media/118-1.mp4"
-      }
-    ]
-  },
+
   {
     "number": "158",
     "note": "照片 2",
